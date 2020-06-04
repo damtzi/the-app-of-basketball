d56212ed8a3ebb3ec3907db42b46a5981093a987
Damian Tziamtzis <d.tziamtzis@lancaster.ac.uk>
2020-06-04 13:25:41 +0200

    Added a new favicon and started working on the player card

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { searchedPlayer } from "./PlayerAtom";
+import { PlayerCard, PlayerBio } from "./styled/Card";
 
 function Profile() {
   const [player, setPlayer] = useRecoilState(searchedPlayer);
+  const [playerBio, setPlayerBio] = useState();
+  const [isLoading, setLoading] = useState(true);
 
-  return (
+  const fetchData = async () => {
+    setLoading(false);
+    const [firstName, lastName] = player.split(" ");
+    try {
+      const response = await fetch(
+        `https://www.balldontlie.io/api/v1/players?search=${firstName}%20${lastName}`
+      );
+      const data = await response.json();
+      setPlayerBio(data.data);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    if (player !== "") {
+      fetchData();
+    }
+  }, [player]);
+
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <div>
-      <h1>{player}</h1>
+      <PlayerCard>
+        <PlayerBio>{player}</PlayerBio>
+      </PlayerCard>
+      {console.log(playerBio)}
     </div>
   );
 }
---
+++ src/components/styled/Card.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PlayerCard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 250px;
+  margin-top: 50px;
+`;
+
+export const PlayerBio = styled.div`
+  background-color: #fff;
+  text-align: center;
+  width: 25%;
+  height: 100%;
+  box-shadow: 2px 2px 5px;
+`;
